1eba25b242f952856624399cb50e85d2386bf6de
Carl Vuorinen <carl.vuorinen@gmail.com>
2019-07-30 12:21:44 +0300

    Subscription handling
    Save subscriptions to jsonbin.io
    Allow subscribe/unsubscribe by mention @botName

--- src/functions/notifica.js
+++ src/functions/notifica.js
@@ -1,63 +1,94 @@
 import fetch from "node-fetch";
 
+const botName = "NotificaBot";
+
 exports.handler = async function(event) {
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
 
-  const params = JSON.parse(event.body);
-  //console.log(params);
+  try {
+    const params = JSON.parse(event.body);
+    //console.log(params);
 
-  /**
-   * Format of the incoming params:
-   * {
-   *    group: {
-   *      id: 'uuid-of-the-guild',
-   *      name: 'Guild/Party Name'
-   *    },
-   *    chat: {
-   *      id: 'uuid-of-the-message',
-   *      text: 'Message text',
-   *      timestamp: '2019-01-01T01:01:01.599Z',
-   *      uuid: 'uuid-of-the-sender-user',
-   *      user: 'Sender display name',
-   *      username: 'Sender username',
-   *      ...
-   *    },
-   *    webhookType: 'groupChatReceived',
-   *    user: { _id: 'uuid-of-the-webhook-owner-user' }
-   * }
-   */
-  if (
-    !params ||
-    !params.group ||
-    !params.group.name ||
-    !params.chat ||
-    !params.chat.text ||
-    !params.chat.user
-  ) {
-    return { statusCode: 400, body: "Bad Request" };
+    /**
+     * Format of the incoming params:
+     * {
+     *    group: {
+     *      id: 'uuid-of-the-guild',
+     *      name: 'Guild Name'
+     *    },
+     *    chat: {
+     *      id: 'uuid-of-the-message',
+     *      text: 'Message text',
+     *      timestamp: '2019-01-01T01:01:01.599Z',
+     *      uuid: 'uuid-of-the-sender-user',
+     *      user: 'Sender display name',
+     *      username: 'Sender username',
+     *      ...
+     *    },
+     *    webhookType: 'groupChatReceived',
+     *    user: { _id: 'uuid-of-the-webhook-owner-user' }
+     * }
+     */
+    if (
+      !params ||
+      !params.group ||
+      !params.group.name ||
+      !params.chat ||
+      !params.chat.text ||
+      !params.chat.user
+    ) {
+      return { statusCode: 400, body: "Bad Request" };
+    }
+
+    const { subscriptions } = await getSubscriptions();
+
+    if (!subscriptions || !Array.isArray(subscriptions)) {
+      return { statusCode: 500, body: "Server Error" };
+    }
+
+    if (params.chat.text.includes(`@${botName} subscribe`)) {
+      if (subscriptions.find(sub => sub.id === params.chat.uuid)) {
+        console.log(`already subscribed [uuid:${params.chat.uuid}]`); // TODO response?
+      } else {
+        subscriptions.push({
+          id: params.chat.uuid,
+          name: params.chat.username
+        });
+        await saveSubscriptions(subscriptions);
+      }
+    }
+
+    if (params.chat.text.includes(`@${botName} unsubscribe`)) {
+      if (subscriptions.find(sub => sub.id === params.chat.uuid)) {
+        await saveSubscriptions(
+          subscriptions.filter(sub => sub.id !== params.chat.uuid)
+        );
+      } else {
+        console.log(`subscription not found [uuid:${params.chat.uuid}]`); // TODO response?
+      }
+    }
+
+    const promises = subscriptions
+      .filter(user => params.chat.text.includes(`@${user.name}`))
+      .map(user => {
+        const message = `Hey there **${user.name}**,  \nUser @${
+          params.chat.user
+        } mentioned you in Guild chat at [${
+          params.group.name
+        }](https://habitica.com/groups/guild/${params.group.id})
+
+\`This notification was brought to you by ${botName}\``;
+        return sendMessage(message, user.id)
+          .then(() => console.log(`notified ${user.name}`))
+          .catch(err => console.error(err));
+      });
+
+    await Promise.all(promises);
+  } catch (err) {
+    console.error(err);
   }
-
-  // TODO save in some db etc.
-  const subscriptions = JSON.parse(process.env.SUBSCRIPTIONS);
-
-  const promises = subscriptions
-    .filter(user => params.chat.text.includes(`@${user.name}`))
-    .map(user => {
-      const message = `Hey there **${user.name}**,  \nUser @${
-        params.chat.user
-      } mentioned you in Guild chat at [${
-        params.group.name
-      }](https://habitica.com/groups/guild/${params.group.id})
-
-\`This notification was brought to you by NotificaBot\``;
-      return sendMessage(message, user.id)
-        .then(() => console.log(`notified ${user.name}`))
-        .catch(err => console.error(err));
-    });
-
-  await Promise.all(promises);
 
   return {
     statusCode: 200,
@@ -65,32 +96,59 @@ exports.handler = async function(event) {
   };
 };
 
-async function sendMessage(message, userId) {
-  try {
-    const url = process.env.API_BASE_URL + "/members/send-private-message";
-    const options = {
-      method: "POST",
-      body: JSON.stringify({ message, toUserId: userId }),
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "x-api-user": process.env.USER_ID,
-        "x-api-key": process.env.API_KEY
-      }
-    };
-
-    //console.log("fetch", { url, options });
-
-    const response = await fetch(url, options);
-
-    if (!response.ok) {
-      throw Error(
-        `${response.status} ${response.statusText} "${await response.text()}"`
-      );
+async function getSubscriptions() {
+  const url = process.env.JSONBIN_URL + "/latest";
+  const options = {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "secret-key": process.env.JSONBIN_KEY
     }
+  };
 
-    return response.json();
-  } catch (err) {
-    return err;
+  return fetchJson(url, options);
+}
+
+async function saveSubscriptions(subscriptions) {
+  const url = process.env.JSONBIN_URL;
+  const options = {
+    method: "PUT",
+    body: JSON.stringify({ subscriptions }),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "secret-key": process.env.JSONBIN_KEY
+    }
+  };
+
+  return fetchJson(url, options);
+}
+
+async function sendMessage(message, userId) {
+  const url = process.env.API_BASE_URL + "/members/send-private-message";
+  const options = {
+    method: "POST",
+    body: JSON.stringify({ message, toUserId: userId }),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "x-api-user": process.env.USER_ID,
+      "x-api-key": process.env.API_KEY
+    }
+  };
+
+  return fetchJson(url, options);
+}
+
+async function fetchJson(url, options) {
+  //console.log("fetch", { url, options });
+  const response = await fetch(url, options);
+
+  if (!response.ok) {
+    throw Error(
+      `${response.status} ${response.statusText} "${await response.text()}"`
+    );
   }
+
+  return response.json();
 }
